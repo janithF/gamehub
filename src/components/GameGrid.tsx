ee66e820a4f96e08f66080ade2bb73a246170a42
Janith Fernando <janithnfernando@gmail.com>
2023-11-12 23:16:09 +0530

    finetunr the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid spacing={5} columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}>
+      <SimpleGrid spacing={6} columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
